dbe0335377282f7d01d041ebba52f000ba88a946
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-01-30 16:19:05 +0100

    Create Assignment1Test.java

---
+++ Assignment1/Assignment1Test.java
@@ -0,0 +1,41 @@
+package main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+
+@SuppressWarnings("unused")
+public class Assignment1Test {
+	
+	@Test
+	public void testMoveForward(){
+		
+		
+	}
+	
+	@Test 
+	public void testPark(){
+		
+	}
+	
+	@Test
+	public void testUnpark(){
+		
+	}
+	
+	@Test
+	public void testIsEmpty(){
+		
+	}
+	
+	@Test
+	public void testMoveBackward(){
+		
+	}
+
+	@Test
+	public void testPosition(){
+		
+	}
+}
