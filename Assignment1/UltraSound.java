1cc81a0c6e2e904fc188983f06964b8050633264
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-01-30 13:35:19 +0100

    Create UltraSound.java

---
+++ Assignment1/UltraSound.java
@@ -0,0 +1,15 @@
+package main;
+
+public class UltraSound {
+
+	public int checkDistance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean veryNoisy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
