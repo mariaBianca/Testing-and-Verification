975487874b8ef51cb34cb34136d0adf3a1096916
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-02-10 08:19:53 +0100

    Update CarInterface.java

--- Assignment1/main/CarInterface.java
+++ Assignment1/main/CarInterface.java
@@ -13,21 +13,24 @@ public interface CarInterface {
      * through the isEmpty method.
      * @return Position(data structure)
      */
-    public Position moveForward(Ultrasound ultrasoundOne, Ultrasound ultrasoundTwo) throws IllegalArgumentException;
+    public Position moveForward(UltrasoundSensor ultrasoundOne, UltrasoundSensor ultrasoundTwo) 
+    		throws IllegalArgumentException;
 
     /**
      * Query the two ultrasound sensors and filter
      * the noise in their result .
      * @return distanceToObject (to the nearest object in the right hand sight)
      */
-    public double isEmpty(Ultrasound ultrasoundOne, Ultrasound ultrasoundTwo) throws IllegalArgumentException;
+    public double isEmpty(UltrasoundSensor ultrasoundOne, UltrasoundSensor ultrasoundTwo)
+    		throws IllegalArgumentException;
 
     /**
      * The car moves backward with one meter, by querying the ultrasound sensors
      * through the isEmpty method.
      * @return Position (of the car)
      */    
-    public Position moveBackward(Ultrasound ultrasoundOne, Ultrasound ultrasoundTwo) throws IllegalArgumentException;
+    public Position moveBackward(UltrasoundSensor ultrasoundOne, UltrasoundSensor ultrasoundTwo) 
+    		throws IllegalArgumentException;
 	
     /**
      *The car moves to the beginning of the current 5 meter free stretch of parking place, or until
