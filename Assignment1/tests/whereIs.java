8c13f962c04c2c138d70249c5c4824338f122763
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-02-13 13:31:54 +0100

    Create whereIs.java

---
+++ Assignment1/tests/whereIs.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * WhereIs(): 
+ * Description: The position of the car is determined in the street, as well as its situation. 
+ * Pre-condition: The car must be on the street. 
+ * Post_condition: The position and situation of the car are detected.
+ * Test-cases:
+ * TC1. The cars’ positions isn’t beyond the end of the street.
+ * TC2. The cars’ positions isn’t beyond the beginning of the street.
+ * TC3. The car is parked.
+ * */
+public class whereIs {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
