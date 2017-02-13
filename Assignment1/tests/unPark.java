f1ac8301f45da2bbca656c0b4522cdf6205ed2e2
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-02-13 13:38:41 +0100

    Create unPark.java

---
+++ Assignment1/tests/unPark.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * Description: The car moves forward (and to the left) to the front of the parking space if the car is parked. 
+ * Pre-condition: The car must be parked. 
+ * Post_condition: The car has moved forward and to the left.
+ * Test-cases: 
+ *           TC1. The car is parked.
+ *           TC2. Car is not parked.
+ *           TC3. Car is not on the street.
+ */
+public class unPark {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
