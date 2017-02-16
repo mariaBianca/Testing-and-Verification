9acf2932398ca94e269f1c7b19a17db5bf143d18
Ninosauleo <ninosauleo@gmail.com>
2017-02-16 21:23:28 +0100

    mockedTest

---
+++ Assignment2/Assignment1/tests/tests/parkMockedTests.java
@@ -0,0 +1,89 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+import main.Car;
+import main.Position;
+import newErrorHandling.StreetLengthException;
+
+public class parkMockedTests {
+
+	
+	@Mock
+	Position position;
+	private Car car; //We are re-using the class by making it a Field
+	
+	@Before
+	public void setup(){
+		/**
+		 * Initialize the mocks
+		 */
+        MockitoAnnotations.initMocks(this);
+        car = new Car(null, null, 0, false, false);
+    }
+	 
+	 /**
+     * TC1 The car executes the pre-programmed reverse parallel parking.
+     */
+    @Test
+    public void carShouldExecutePreProgrammedPark() throws StreetLengthException {
+    	car.park(2, 2);
+    	/**
+    	 * Set up the expectations
+    	 */
+    	when(position.getParkingStatus()).thenReturn(true);
+    	Boolean result = position.getParkingStatus();
+    	assertTrue(result);
+    	/**
+    	 * To verify that the mocking worked we use a static method from the class Mockito
+    	 * 
+    	 */
+    	verify(position).getParkingStatus();
+    }
+    
+    /**
+     * TC2 The car is already parked
+     */
+    @Test
+    public void carisNotParked() throws StreetLengthException {
+    	Car car = new Car(null, null, 0, true, false);
+    	car.park(2, 2);
+    	/**
+    	 * Set up the expectations
+    	 */
+    	when(position.getParkingStatus()).thenReturn(false);
+    	Boolean result = position.getParkingStatus();
+    	assertFalse(result);
+    	/**
+    	 * To verify that the mocking worked we use a static method from the class Mockito
+    	 * 
+    	 */
+    	verify(position).getParkingStatus();
+    }
+    
+    
+    /**
+     * TC3 the car never finds parking spot
+     */
+    
+    @SuppressWarnings("unchecked")
+	@Test(expected = StreetLengthException.class)
+    public void isEmptyShouldThrowAnExceptionDistanceoutOfScope() throws StreetLengthException {
+        car.park(3,3);
+    	/**
+    	 * Set up the expectations, we don't need to verify or assert any value.
+    	 */
+    	when(position.getParkingStatus()).thenThrow(StreetLengthException.class);
+    	
+    }
+    
+
+}
