3649625bc510eaa8775bdcc93f0118dad08f95d7
Ninosauleo <ninosauleo@gmail.com>
2017-02-17 14:22:35 +0100

    This will work with the change in the class car and carImpl

--- Assignment2/Assignment1/mockedTests/MockScenarioTest.java
+++ Assignment2/Assignment1/mockedTests/MockScenarioTest.java
@@ -13,50 +13,51 @@ import org.mockito.Spy;
 import static org.mockito.Mockito.*;
 
 import main.Car;
+import main.CarImpl;
 import main.Position;
 import main.UltrasoundSensor;
 import newErrorHandling.StreetLengthException;
 
 public class MockScenarioTest {
-
-	
-	@Mock
-	Position position;
-	private Car car; //We are re-using the class by making it a Field
-	
-	
-	@Before
-	public void setup(){
-		/**
-		 * Initialize the mocks
-		 */
+    
+    
+    @Mock
+    Position position;
+    private CarImpl car; //We are re-using the class by making it a Field
+    
+    
+    @Before
+    public void setup(){
+        /**
+         * Initialize the mocks
+         */
         MockitoAnnotations.initMocks(this);
-        car = new Car(null, null, 0, false, false);
+        car = new CarImpl(null, null, 0, false, false);
     }
-	 
-	/**
+    
+    /**
      * Scenario 1 - The car moves until it finds an empty parking spot.
      * The car parks when it finds the empty spot.
      * Starting from position 0;
      */
     @Test
     public void carShouldExecutePreProgrammedPark() throws StreetLengthException {
-    	//moves the car forward until a spot is found
-    	//when a spot is found, it moves forward for 5m and parks in the spot
-    	car.park(2, 2);
-    	/**
-    	 * Set up the expectations
-    	 */
-    	
-    	//checks if the car is parked
-    	when(position.getParkingStatus()).thenReturn(true);
-    	Boolean result = position.getParkingStatus();
-    	assertTrue(result);
-    	/**
-    	 * To verify that the mocking worked we use a static method from the class Mockito
-    	 * 
-    	 */
-    	verify(position).getParkingStatus();
+        //moves the car forward until a spot is found
+        //when a spot is found, it moves forward for 5m and parks in the spot
+        car.park(2, 2);
+        /**
+         * Set up the expectations
+         */
+        
+        //checks if the car is parked
+        when(position.getParkingStatus()).thenReturn(true);
+        Boolean result = position.getParkingStatus();
+        assertTrue(result);
+        /**
+         * To verify that the mocking worked we use a static method from the class Mockito
+         *
+         */
+        verify(position).getParkingStatus();
     }
     
     /**
@@ -65,34 +66,34 @@ public class MockScenarioTest {
      */
     @Test
     public void carUnparksAndMoves() throws StreetLengthException {
-    	Car car = new Car(null, null, 0, true, false);
-    	UltrasoundSensor uOne = new UltrasoundSensor();
-    	UltrasoundSensor uTwo = new UltrasoundSensor();
-    	
-    	//check if car is parked
-    	when(position.getParkingStatus()).thenReturn(true);
-    	
-    	//unpark car
-    	car.unpark();
-    	when(position.getParkingStatus()).thenReturn(false);
-    	
-    	//go to the end of the street
-    	when(position.getPositionOnStreet()).thenReturn(499);
-    	int intRes = position.getPositionOnStreet();
-    	assertEquals(499, intRes);
-    	verify(position).getPositionOnStreet();
-    	
-    	//go back 10 meters and try to park the car again
-    	when(position.getPositionOnStreet()).thenReturn(490);
-    	position = car.moveBackward(position);
-    	car.park(2, 2);
-    	when(position.getParkingStatus()).thenReturn(true);
-    	
-    	/**
-    	 * To verify that the mocking worked we use a static method from the class Mockito
-    	 * 
-    	 */
-    	verify(position).getParkingStatus();
+        Car car = new CarImpl(null, null, 0, true, false);
+        UltrasoundSensor uOne = new UltrasoundSensor();
+        UltrasoundSensor uTwo = new UltrasoundSensor();
+        
+        //check if car is parked
+        when(position.getParkingStatus()).thenReturn(true);
+        
+        //unpark car
+        car.unpark();
+        when(position.getParkingStatus()).thenReturn(false);
+        
+        //go to the end of the street
+        when(position.getPositionOnStreet()).thenReturn(499);
+        int intRes = position.getPositionOnStreet();
+        assertEquals(499, intRes);
+        verify(position).getPositionOnStreet();
+        
+        //go back 10 meters and try to park the car again
+        when(position.getPositionOnStreet()).thenReturn(490);
+        position = car.moveBackward(position);
+        car.park(2, 2);
+        when(position.getParkingStatus()).thenReturn(true);
+        
+        /**
+         * To verify that the mocking worked we use a static method from the class Mockito
+         *
+         */
+        verify(position).getParkingStatus();
     }
     
     
@@ -109,31 +110,31 @@ public class MockScenarioTest {
      */
     
     @SuppressWarnings("unchecked")
-	@Test(expected = StreetLengthException.class)
+    @Test(expected = StreetLengthException.class)
     public void isEmptyShouldThrowAnExceptionDistanceoutOfScope() throws StreetLengthException {
-    	
-    	when(position.getPositionOnStreet()).thenReturn(-15);
-    	//whereIs doesn't allow out-of-Bounds check
-    	car.getPosition();
-
-    	car.setPosition(2);
-    	car.whereIs();
-
-    	
-    	car.park(2,2);
-    	car.park(3,3);
-    	car.unpark();
-    	car.unpark();
-    	car.moveBackward();
-    	car.park(3,3);
-    	verify(position).getParkingStatus();
-    	
-    	//TODO: SENSORS RANDOM CHECK && OUT OF BOUNDS CHECK && CAR OUT OF BOUNDS CHECK
-    	
-    	/**
-    	 * Set up the expectations, we don't need to verify or assert any value.
-    	 */
-    	when(position.getParkingStatus()).thenThrow(StreetLengthException.class);
-    	
+        
+        when(position.getPositionOnStreet()).thenReturn(-15);
+        //whereIs doesn't allow out-of-Bounds check
+        car.getPosition();
+        
+        car.setPosition(2);
+        car.whereIs();
+        
+        
+        car.park(2,2);
+        car.park(3,3);
+        car.unpark();
+        car.unpark();
+        car.moveBackward();
+        car.park(3,3);
+        verify(position).getParkingStatus();
+        
+        //TODO: SENSORS RANDOM CHECK && OUT OF BOUNDS CHECK && CAR OUT OF BOUNDS CHECK
+        
+        /**
+         * Set up the expectations, we don't need to verify or assert any value.
+         */
+        when(position.getParkingStatus()).thenThrow(StreetLengthException.class);
+        
     }
 }
